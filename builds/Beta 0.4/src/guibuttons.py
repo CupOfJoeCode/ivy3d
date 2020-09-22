3aa7f3a00fe0c5ced1cf242f0c1506faa7b82d9a
CupOfJoeCode <62409275+CupOfJoeCode@users.noreply.github.com>
2020-09-22 16:27:06 -0400

    Add files via upload

---
+++ builds/Beta 0.4/src/guibuttons.py
@@ -0,0 +1,254 @@
+buttons = [
+        {
+            "name":"Select Object",
+            "x":2,
+            "y":70,
+            "pressed":False
+            
+        },
+        {
+            "name":"Change Geometry",
+            "x":2,
+            "y":90,
+            "pressed":False
+            
+        },
+        {
+            "name":"x-",
+            "x":2,
+            "y":110,
+            "pressed":False 
+        },
+        {
+            "name":"x+",
+            "x":40,
+            "y":110,
+            "pressed":False 
+        },
+        {
+            "name":"y-",
+            "x":2,
+            "y":130,
+            "pressed":False 
+        },
+        {
+            "name":"y+",
+            "x":40,
+            "y":130,
+            "pressed":False 
+        },
+        {
+            "name":"z-",
+            "x":2,
+            "y":150,
+            "pressed":False 
+        },
+        {
+            "name":"z+",
+            "x":40,
+            "y":150,
+            "pressed":False 
+        },
+
+
+        {
+            "name":"l-",
+            "x":60,
+            "y":110,
+            "pressed":False 
+        },
+        {
+            "name":"l+",
+            "x":90,
+            "y":110,
+            "pressed":False 
+        },
+        {
+            "name":"h-",
+            "x":60,
+            "y":130,
+            "pressed":False 
+        },
+        {
+            "name":"h+",
+            "x":90,
+            "y":130,
+            "pressed":False 
+        },
+        {
+            "name":"w-",
+            "x":60,
+            "y":150,
+            "pressed":False 
+        },
+        {
+            "name":"w+",
+            "x":90,
+            "y":150,
+            "pressed":False 
+        },
+
+
+
+
+        {
+            "name":"r-",
+            "x":2,
+            "y":180,
+            "pressed":False 
+        },
+        {
+            "name":"r+",
+            "x":40,
+            "y":180,
+            "pressed":False 
+        },
+        {
+            "name":"g-",
+            "x":2,
+            "y":200,
+            "pressed":False 
+        },
+        {
+            "name":"g+",
+            "x":40,
+            "y":200,
+            "pressed":False 
+        },
+        {
+            "name":"b-",
+            "x":2,
+            "y":220,
+            "pressed":False 
+        },
+        {
+            "name":"b+",
+            "x":40,
+            "y":220,
+            "pressed":False 
+        },
+
+        {
+            "name":"Toggle Rigidbody",
+            "x":2,
+            "y":240,
+            "pressed":False 
+        },
+        {
+            "name":"Toggle CharacterController",
+            "x":2,
+            "y":260,
+            "pressed":False 
+        },
+
+        
+
+
+        {
+            "name":"Bounce-",
+            "x":2,
+            "y":280,
+            "pressed":False 
+        },
+        {
+            "name":"Bounce+",
+            "x":2,
+            "y":300,
+            "pressed":False 
+        },
+        {
+            "name":"Friction-",
+            "x":2,
+            "y":320,
+            "pressed":False 
+        },
+        {
+            "name":"Friction+",
+            "x":2,
+            "y":340,
+            "pressed":False 
+        },
+
+        {
+            "name":"Change Texture",
+            "x":2,
+            "y":360,
+            "pressed":False 
+        },
+
+
+        {
+            "name":"Add Mesh",
+            "x":2,
+            "y":380,
+            "pressed":False 
+        },
+        {
+            "name":"Remove Mesh",
+            "x":2,
+            "y":400,
+            "pressed":False 
+        },
+
+        {
+            "name":"Render and Export",
+            "x":2,
+            "y":420,
+            "pressed":False 
+        },
+
+        {
+            "name":"Render Video",
+            "x":2,
+            "y":440,
+            "pressed":False 
+        },
+
+        {
+            "name":"Toggle Wireframe",
+            "x":2,
+            "y":460,
+            "pressed":False 
+        },
+
+
+        {
+            "name":"Load",
+            "x":2,
+            "y":2,
+            "pressed":False 
+        },
+
+        {
+            "name":"Save",
+            "x":2,
+            "y":22,
+            "pressed":False 
+        },
+        {
+            "name":"RotX-",
+            "x":2,
+            "y":480,
+            "pressed":False 
+        },
+        {
+            "name":"RotX+",
+            "x":2,
+            "y":500,
+            "pressed":False 
+        },
+        {
+            "name":"RotY-",
+            "x":2,
+            "y":520,
+            "pressed":False 
+        },
+        {
+            "name":"RotY+",
+            "x":2,
+            "y":540,
+            "pressed":False 
+        },
+
+
+    ]
